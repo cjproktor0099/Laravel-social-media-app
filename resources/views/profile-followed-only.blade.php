aace26a53090866b56f4cd15598394b06e26d013
cjproktor0099 <111074467+cjproktor0099@users.noreply.github.com>
2023-04-01 16:21:42 +0800

    fix followed users list
    change userfollowing into followeduser to display the correct username and avatar you follow

--- resources/views/profile-followed-only.blade.php
+++ resources/views/profile-followed-only.blade.php
@@ -1,8 +1,8 @@
 <div class="list-group">
     @foreach ($followed as $followed)
-    <a href="/profile/{{$followed->userFollowing->id}}" class="list-group-item list-group-item-action">
-        <img class="avatar-tiny" src="{{$followed->userFollowing->avatar}}" />
-        <strong>{{$followed->userFollowing->username}}</strong> on {{$followed->userFollowing->created_at->format('M-d-Y')}}
+    <a href="/profile/{{$followed->followedUser->id}}" class="list-group-item list-group-item-action">
+        <img class="avatar-tiny" src="{{$followed->followedUser->avatar}}" />
+        <strong>{{$followed->followedUser->username}}</strong> on {{$followed->followedUser->created_at->format('M-d-Y')}}
       </a>
     @endforeach
 </div>
